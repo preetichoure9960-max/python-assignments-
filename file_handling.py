35b411f69fc16cdd5715f2a19edcf1c4a0d7a7ff
PREETI CHOURE <preetichoure9960@gmail.com>
2026-04-17 21:23:20 +0530

    python

---
+++ file_handling.py
@@ -0,0 +1,11 @@
+
+try:
+    file = open("data.txt", "r")
+    data = file.read()
+    print(data)
+    file.close()
+except FileNotFoundError:
+    print("File not found")
+    with open("data.txt", "r") as file:
+        data = file.read()
+        print(data)
